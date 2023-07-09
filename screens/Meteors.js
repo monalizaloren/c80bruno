e77d681822ea17d05172be4fc10a4f65643a6659
Monaliza Loren <83433815+monalizaloren@users.noreply.github.com>
2023-07-09 16:31:44 -0300

    Update Meteors.js

--- screens/Meteors.js
+++ screens/Meteors.js
@@ -22,37 +22,7 @@ export default class MeteorScreen extends Component {
         };
     }
 
-    componentDidMount() {
-        this.getMeteors()
-    }
-
-    getMeteors = () => {
-        axios
-            .get("https://api.nasa.gov/neo/rest/v1/feed?api_key=nAkq24DJ2dHxzqXyzfdreTvczCVOnwJuFLFq4bDZ")
-            .then(response => {
-                this.setState({ meteors: response.data.near_earth_objects })
-            })
-            .catch(error => {
-                Alert.alert(error.message)
-            })
-    }
-
-    renderItem = ({ item }) => {
-        let meteor = item
-        let bg_img, speed, size;
-        if (meteor.threat_score <= 30) {
-            bg_img = require("../assets/meteor_bg1.png")
-            speed = require("../assets/meteor_speed3.gif")
-            size = 100
-        } else if (meteor.threat_score <= 75) {
-            bg_img = require("../assets/meteor_bg2.png")
-            speed = require("../assets/meteor_speed3.gif")
-            size = 150
-        } else {
-            bg_img = require("../assets/meteor_bg3.png")
-            speed = require("../assets/meteor_speed3.gif")
-            size = 200
-        }
+   
         return (
             <View>
                 <ImageBackground source={bg_img} style={styles.backgroundImage}>
